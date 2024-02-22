741e2eb15c2561db8149fbc30d250be4041be23a
Matthew Pacey <paceym@oregonstate.edu>
2024-02-22 14:19:38 -0800

    Updated to handle day boundaries better

--- scripts/generate_walk_intervals.py
+++ scripts/generate_walk_intervals.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 from datetime import datetime, timedelta
 
+HOUR_OFFSET = -7         # number of hours to add to account for timezone difference
+WINDOW_MINS = 10        # number of minutes on either side of window to show
+
 # Get the parent directory of the current script's directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)
@@ -22,27 +25,32 @@ def process_csv(file_path, output_df):
     latest_date = df['Date'].max()
     
     # Print date window
-    print(f"Date window for {file_path}: {earliest_date.strftime('%m/%d/%Y')} - {latest_date.strftime('%m/%d/%Y')}")
+    print(f"Date window for {os.path.basename(file_path)}: {earliest_date.strftime('%m/%d/%Y')} - {latest_date.strftime('%m/%d/%Y')}")
     
         
     for index, row in df.iterrows():
         # Extracting relevant columns
         animal_id = int(row['ID'][1:])
         sex = row['ID'][0]
-        start_date = row['Date'] # datetime.strptime(row['Date'], '%d-%b-%y') # Parse date in "18-Dec-20" format        
+        start_date = row['Date']
         start_time = datetime.strptime(row['Time'], '%H:%M:%S')
-        end_time = start_time + timedelta(minutes=10)
-        start_time_minus_5 = start_time - timedelta(minutes=5)
+        # check if the previous or next day is needed
+        if HOUR_OFFSET < 0 and start_time.hour < abs(HOUR_OFFSET):
+            start_date -= timedelta(hours=24)
+        elif HOUR_OFFSET > 0 and start_time.hour > 24 - HOUR_OFFSET:
+            start_date += timedelta(hours=24)
+        start_time += timedelta(hours=HOUR_OFFSET)        
+        end_time = start_time + timedelta(minutes=WINDOW_MINS)
+        start_time_window = start_time - timedelta(minutes=WINDOW_MINS)
         marker_time_1 = start_time
         
         # Generating Kill_ID
         kill_id = output_df['Kill_ID'].max() + 1 if len(output_df) > 0 else 1000        
-        # Appending to output dataframe
         output_df = output_df._append({'AnimalID': animal_id,
                                     'Sex': sex,
                                     'Kill_ID': kill_id,
                                     'Start Date': start_date.strftime('%m/%d/%Y'),
-                                    'Start time': start_time_minus_5.strftime('%H:%M:%S'),
+                                    'Start time': start_time_window.strftime('%H:%M:%S'),
                                     'End time': end_time.strftime('%H:%M:%S'),
                                     'MarkerTime1': marker_time_1.strftime('%H:%M:%S')}, 
                                     ignore_index=True)
@@ -58,3 +66,4 @@ for file_name in os.listdir(input_dir):
 
 # Writing to output CSV
 output_df.to_csv(output_file, index=False)
+print(f"Collated entries into {output_file}")
